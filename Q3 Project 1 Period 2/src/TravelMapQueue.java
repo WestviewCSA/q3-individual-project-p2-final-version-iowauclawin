74861052a8e494034fb17bb3049f229ca4756029
1922537 <1922537@01-18135.pusd.dom>
2026-03-20 12:00:25 -0700

    I added more to my queue code

--- Q3 Project 1 Period 2/src/TravelMapQueue.java
+++ Q3 Project 1 Period 2/src/TravelMapQueue.java
@@ -24,18 +24,132 @@ public class TravelMapQueue {
 					String[] word = r.nextLine().split(" ");
 					for(int j = 0; j < column; j++) {
 						map[i][j][f] = new Cord(j, i, word[j]);
-						if(word[j].equals("W")) {
-							y = i;
-							x = j;
-						}
 					}
 				}
 			}
 			Queue<Integer> rows = new LinkedList<>();
 			Queue<Integer> columns = new LinkedList<>();
 			Queue<String> symbol = new LinkedList<>();
+
 			
-		
+			for(int f = 0; f < rooms; f++) {
+				for(int i = 0;;i++) {
+					if(i == 0) {
+						for(int j = 0; j < row; j++) {
+							for(int k = 0; k < column; k++) {
+								if(map[j][k][f].getSymbol().equals("W")) {
+									rows.offer(y);
+									columns.offer(x);
+									symbol.offer("W");
+									map[j][k][f].setVisit(true);
+								}
+							}
+						}
+					}
+					//This is for North
+					if(rows.peek()-1 >= 0 && map[rows.peek()-1][columns.peek()][f].getVisit() == false) {
+						if(map[rows.peek()-1][columns.peek()][f].getSymbol().equals(".")) {
+							map[rows.peek()-1][columns.peek()][f].setPrev(map[rows.peek()][columns.peek()][f]);
+							map[rows.peek()-1][columns.peek()][f].setVisit(true);
+							rows.offer(rows.peek()-1);
+							columns.offer(columns.peek());
+							symbol.offer(".");
+							
+						}
+						else if(map[rows.peek()-1][columns.peek()][f].getSymbol().equals("$") || map[rows.peek()-1][columns.peek()][f].getSymbol().equals("|")) {
+							map[rows.peek()-1][columns.peek()][f].setPrev(map[rows.peek()][columns.peek()][f]);
+							rows.offer(rows.peek()-1);
+							columns.offer(columns.peek());
+							if(f == rooms-1) {
+								symbol.offer("$");
+							}
+							else {
+								symbol.offer("|");
+							}
+							break;
+						}	
+					}
+					
+					//This is for South
+					if(rows.peek()+1 < row && map[rows.peek()+1][columns.peek()][f].getVisit() == false) {
+						if(map[rows.peek()+1][columns.peek()][f].getSymbol().equals(".")) {
+							map[rows.peek()+1][columns.peek()][f].setPrev(map[rows.peek()][columns.peek()][f]);
+							map[rows.peek()+1][columns.peek()][f].setVisit(true);
+							rows.offer(rows.peek()+1);
+							columns.offer(columns.peek());
+							symbol.offer(".");
+							
+						}
+						else if(map[rows.peek()+1][columns.peek()][f].getSymbol().equals("$") || map[rows.peek()+1][columns.peek()][f].getSymbol().equals("|")) {
+							map[rows.peek()+1][columns.peek()][f].setPrev(map[rows.peek()][columns.peek()][f]);
+							rows.offer(rows.peek()+1);
+							columns.offer(columns.peek());
+							if(f == rooms-1) {
+								symbol.offer("$");
+							}
+							else {
+								symbol.offer("|");
+							}
+							break;
+						}	
+					}
+					
+					//This is for East
+					if(columns.peek()+1 < column && map[rows.peek()][columns.peek()+1][f].getVisit() == false) {
+						if(map[rows.peek()][columns.peek()+1][f].getSymbol().equals(".")) {
+							map[rows.peek()][columns.peek()+1][f].setPrev(map[rows.peek()][columns.peek()][f]);
+							map[rows.peek()][columns.peek()+1][f].setVisit(true);
+							rows.offer(rows.peek());
+							columns.offer(columns.peek()+1);
+							symbol.offer(".");
+							
+						}
+						else if(map[rows.peek()][columns.peek()+1][f].getSymbol().equals("$") || map[rows.peek()][columns.peek()+1][f].getSymbol().equals("|")) {
+							map[rows.peek()][columns.peek()+1][f].setPrev(map[rows.peek()][columns.peek()][f]);
+							rows.offer(rows.peek());
+							columns.offer(columns.peek()+1);
+							if(f == rooms-1) {
+								symbol.offer("$");
+							}
+							else {
+								symbol.offer("|");
+							}
+							break;
+						}	
+					}
+					
+					//This is for West
+					if(columns.peek()-1 >= 0 && map[rows.peek()][columns.peek()-1][f].getVisit() == false) {
+						if(map[rows.peek()][columns.peek()-1][f].getSymbol().equals(".")) {
+							map[rows.peek()][columns.peek()-1][f].setPrev(map[rows.peek()][columns.peek()][f]);
+							map[rows.peek()][columns.peek()-1][f].setVisit(true);
+							rows.offer(rows.peek());
+							columns.offer(columns.peek()-1);
+							symbol.offer(".");
+							
+						}
+						else if(map[rows.peek()][columns.peek()-1][f].getSymbol().equals("$") || map[rows.peek()][columns.peek()-1][f].getSymbol().equals("|")) {
+							map[rows.peek()][columns.peek()-1][f].setPrev(map[rows.peek()][columns.peek()][f]);
+							rows.offer(rows.peek());
+							columns.offer(columns.peek()-1);
+							if(f == rooms-1) {
+								symbol.offer("$");
+							}
+							else {
+								symbol.offer("|");
+							}
+							break;
+						}	
+					}
+					rows.poll();
+					columns.poll();
+					symbol.poll();
+				}
+				rows.clear();
+				columns.clear();
+				symbol.clear();
+				
+			}
 			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -43,4 +157,6 @@ public class TravelMapQueue {
 		}
 		
 	}
+	
+	
 }
